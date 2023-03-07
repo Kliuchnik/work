dd6d9d240e3113f7f4b335f539c93a0e1d730562
romanklucnik <romankliuchnik@icloud.com>
2023-03-07 22:55:34 +0700

    Hello info

--- test.js
+++ test.js
@@ -2,20 +2,15 @@ const test = "hello";
 
 alert("Привет!")
 
-blablablahu
-
-19919191919191
 
 
 
 
-fsdbsxfgncghmgvhmnfgxb
 dfbxzgbfggggggggggggggggggggggggggggnsdfnxzegnxgnx
-bnxgbnx
-bnxd
-gn
-xdgnxdvn xfhm
-fyu,
-i
-k.b
+
+
+
+
+
+b
 333333333333333333333333333
